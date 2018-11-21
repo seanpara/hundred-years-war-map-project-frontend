fa61526deee474021267afe4ff315fdead072681
seanpara <spara77@gmail.com>
2018-11-21 10:39:05 -0500

    added custom map, new functions

--- src/Map.js
+++ src/Map.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 // import MapGL, {NavigationControl} from 'react-map-gl';
-import ReactMapGL from 'react-map-gl';
-import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css';
-import DrawControl from 'react-mapbox-gl-draw';
+import ReactMapGL, {MapController} from 'react-map-gl';
 
 
 const TOKEN = 'pk.eyJ1Ijoic2Vhbi1wYXJhIiwiYSI6ImNqb29lcG50YzBibjMzcGwxMjZibnd3ZnMifQ.DTE1NiDPv4hV7N8xT3xnZA';
@@ -13,35 +11,45 @@ const navStyle = {
   padding: '10px'
 };
 export default class Map extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-        viewport: {
-          latitude: 50,
-          longitude: -3,
-          zoom: 4,
-          bearing: 0,
-          pitch: 0,
-          width: 500,
-          height: 500,
-        }
-      };
-    }
+  // constructor(props) {
+  //     super(props);
+  //     this.state = {
+  //       viewport: {
+  //         latitude: 50,
+  //         longitude: -3,
+  //         zoom: 3,
+  //         bearing: 0,
+  //         pitch: 0,
+  //         width: 500,
+  //         height: 500,
+  //       }
+  //     };
+  //   }
+  //
+  // handleViewPortChange = (viewport) => {
+  //   const {width, height, latitude, longitude, zoom} = viewport;
+  //   this.setState((state) => {
+  //     return {width, height, latitude, longitude, zoom}
+  //   })
+  // }
 
 
   render() {
-      const {viewport} = this.state;
       return (
+        <>
         <ReactMapGL
-          {...viewport}
+           width={400}
+           height={400}
+           latitude={50}
+           longitude={-3}
+           zoom={3.5}
+           onViewStateChange={(viewport) => {
+           const {width, height, latitude, longitude, zoom} = viewport;
+           // call `setState` and use the state to update the map.
+          }}
           mapStyle="mapbox://styles/sean-para/cjooeq11s245t2spc490zj65f"
-          mapboxApiAccessToken={TOKEN}>
-        </ReactMapGL>
+          mapboxApiAccessToken={TOKEN}/>
+          </>
       );
     }
 }// end of map compoent
-
-
-// <div className="nav" style={navStyle} >
-//   <NavigationControl/>
-// </div>
