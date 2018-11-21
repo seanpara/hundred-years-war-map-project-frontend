4254a13ca1fefcd452bdbb336887996a435662de
seanpara <spara77@gmail.com>
2018-11-21 15:50:03 -0500

    added fetch functions and state to App

--- src/App.js
+++ src/App.js
@@ -4,9 +4,40 @@ import './App.css';
 import Map from "./Map.js"
 
 class App extends Component {
+
+  state = {
+    mapData: [],
+    mapDecriptions: []
+  }
+
+  getMapData = () => {
+    fetch("http://localhost:3000/api/v1/maps")
+    .then(r => r.json())
+    .then( (mapRes) => {
+      this.setState((state) => {
+        return {mapData: mapRes}
+      })
+    })
+  }
+
+  getMapDescriptions = () => {
+    fetch("http://localhost:3000/api/v1/map_descriptions")
+    .then(r => r.json())
+    .then( (descriptionRes) => {
+      this.setState((state) => {
+        return {mapDecriptions: descriptionRes}
+      }, () => console.log(this.state.mapDecriptions))
+    })
+  }
+
+  componentDidMount(){
+    this.getMapData()
+    this.getMapDescriptions()
+  }
+
   render() {
     return (
-      <Map/>
+      <Map mapData={this.state.mapData === [] ? null: this.state.mapData}/>
     );
   }
 }
--- src/Map.js
+++ src/Map.js
@@ -1,24 +1,19 @@
 import React, {Component} from 'react';
 // import MapGL, {NavigationControl} from 'react-map-gl';
-import ReactMapGL, {MapController} from 'react-map-gl';
+import ReactMapGL from 'react-map-gl';
 
 
 const TOKEN = 'pk.eyJ1Ijoic2Vhbi1wYXJhIiwiYSI6ImNqb29lcG50YzBibjMzcGwxMjZibnd3ZnMifQ.DTE1NiDPv4hV7N8xT3xnZA';
-const navStyle = {
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  padding: '10px'
-};
+
 export default class Map extends Component {
   constructor(props) {
       super(props);
       this.state = {
         viewport: {
           viewState:{
-            latitude: 50,
-            longitude: -3,
-            zoom: 3.5,
+            latitude: 50.4,
+            longitude: -1.25,
+            zoom: 4.4,
             // bearing: 0,
             // pitch: 0,
             width: 800,
@@ -35,11 +30,13 @@ export default class Map extends Component {
       return (
         <ReactMapGL
            onViewStateChange={(viewport) => {
-             console.log("Viewport is ", viewport)
+             // console.log("Viewport is ", viewport)
            const {width, height, latitude, longitude, zoom} = viewport;
            this.setState({viewport})
           }}
-          mapStyle="mapbox://styles/sean-para/cjooeq11s245t2spc490zj65f"
+          mapStyle={ this.props.mapData[0] == undefined ?
+            "mapbox://styles/sean-para/cjooeq11s245t2spc490zj65f" : this.props.mapData[0].url
+          }
           mapboxApiAccessToken={TOKEN} {...viewState}/>
       );
     }
